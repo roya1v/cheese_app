9dcf271b8d766a94111de9d58f88c297a98ee9e4
Mike JR <mikedalvik@ya.ru>
2022-09-26 23:09:58 +0200

    Play around with layout

--- lib/onboarding/welcome_page.dart
+++ lib/onboarding/welcome_page.dart
@@ -9,13 +9,12 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class WelcomePage extends StatelessWidget {
   const WelcomePage({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
+  Widget getMobileLayout(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Center(
           child: Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.all(16.0),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -24,7 +23,8 @@ class WelcomePage extends StatelessWidget {
                   'Welcome to CheseeApp',
                   style: Theme.of(context).textTheme.headlineLarge,
                 ),
-                const Image(image: AssetImage('assets/cheese.png')),
+                const Expanded(
+                    child: Image(image: AssetImage('assets/cheese.png'))),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
@@ -62,5 +62,75 @@ class WelcomePage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Widget getDesktopLayout(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Column(
+                children: [
+                  Text(
+                    'Welcome to CheseeApp',
+                    style: Theme.of(context).textTheme.headlineLarge,
+                  ),
+                  const Expanded(
+                    child: Image(
+                      image: AssetImage('assets/cheese.png'),
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                      onPressed: (() {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: ((context) {
+                          return BlocProvider(
+                            child: const SignUpPage(),
+                            create: (context) => SignUpBloc(
+                                repository: context.read(),
+                                userRepository: context.read()),
+                          );
+                        })));
+                      }),
+                      child: const Text('Sign Up')),
+                  OutlinedButton(
+                      onPressed: (() {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: ((context) {
+                          return BlocProvider(
+                            child: const SignInPage(),
+                            create: (context) => SignInBloc(
+                                repository:
+                                    context.read<AuthenticationRepository>()),
+                          );
+                        })));
+                      }),
+                      child: const Text('Sign In')),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (MediaQuery.of(context).size.width > 700) {
+      return getDesktopLayout(context);
+    } else {
+      return getMobileLayout(context);
+    }
   }
 }
